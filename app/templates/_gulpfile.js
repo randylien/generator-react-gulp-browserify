8a4749be8dde3bb1071563069f75b1053d999455
Randy Lien <randylien@gmail.com>
2014-03-11 16:02:37 +0800

    add styles task

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -46,6 +46,15 @@ gulp.task('coffee', function () {
 });
 <% } %>
 
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('app/styles/**/*.css')
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+});
+
 // Scripts
 gulp.task('scripts', function () {
     return gulp.src('app/scripts/**/*.js')
