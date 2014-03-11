bebe1ff9fe4c2d400889c98f168a6567857f8207
Randy Lien <randylien@gmail.com>
2014-03-11 11:43:50 +0800

    initialize

---
+++ app/index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var ReactGulpBrowserifyGenerator = yeoman.generators.Base.extend({
+    init: function () {
+        this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.npmInstall();
+            }
+        });
+    },
+
+    askFor: function () {
+        var done = this.async();
+
+        // have Yeoman greet the user
+        console.log(this.yeoman);
+
+        // replace it with a short and sweet description of your generator
+        console.log(chalk.magenta('You\'re using the fantastic React generator. We provide you full JavaScript solution with Sass support!'));
+
+        var prompts = [{
+            type: 'checkbox',
+            name: 'features',
+            message: 'What more would you like?',
+            choices: [{
+                name: 'Sass with Compass',
+                value: 'includeSass',
+                checked: true
+            }, {
+                name: 'Bootstrap',
+                value: 'includeBootstrap',
+                checked: true
+            }, {
+                name: 'Modernizr',
+                value: 'includeModernizr',
+                checked: true
+            }, {
+                name: 'Stylus with nib',
+                value: 'includeStylus',
+                checked: true
+            }, {
+                name: 'CoffeeScript for JavaScript',
+                value: 'includeCoffeeScript',
+                checked: true
+            }]
+        }];
+
+        this.prompt(prompts, function (answers) {
+            var features = answers.features;
+
+            function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+            // manually deal with the response, get back and store the results.
+            // we change a bit this way of doing to automatically do this in the self.prompt() method.
+            this.includeSass = hasFeature('includeSass');
+            this.includeBootstrap = hasFeature('includeBootstrap');
+            this.includeModernizr = hasFeature('includeModernizr');
+            this.includeStylus = hasFeature('includeStylus');
+            this.includeCoffeeScript = hasFeature('includeCoffeeScript');
+
+            done();
+        }.bind(this));
+    },
+
+    app: function () {
+        this.mkdir('app');
+        this.mkdir('app/scripts');
+        this.mkdir('app/styles');
+        this.mkdir('app/images');
+
+        this.template('_package.json', 'package.json');
+        this.copy('_bower.json', 'bower.json');
+    },
+
+    projectfiles: function () {
+        this.copy('editorconfig', '.editorconfig');
+        this.copy('jshintrc', '.jshintrc');
+    }
+});
+
+module.exports = ReactGulpBrowserifyGenerator;
---
+++ app/templates/_gulpfiles.js
@@ -0,0 +1,141 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+<% if (includeSass) { %>
+// Styles
+gulp.task('sass', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.rubySass({
+            style: 'expanded',
+            loadPath: ['app/bower_components']
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+});
+<% } %>
+
+<% if (includeStylus) { %>
+// Stylus compile
+gulp.task('stylus', function () {
+    return gulp.src('app/styles/**/*.styl')
+        .pipe($.stylus({
+            set:['compress']
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+});
+
+<% } %>
+
+<% if (includeCoffeeScript) { %>
+// CoffeeScript
+gulp.task('coffee', function () {
+    return gulp.src('app/coffee/**/*.coffee')
+        .pipe(
+            $.coffee({ bare: true }).on('error', $.util.log)
+        )
+        .pipe(gulp.dest('app/scripts'));
+});
+<% } %>
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.react())
+        .pipe($.jshint('.jshintrc'))
+        .pipe($.jshint.reporter('default'))
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+    });
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size())
+        .pipe($.connect.reload());
+});
+
+// Clean
+gulp.task('clean', function () {
+    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
+});
+
+// Bundle
+gulp.task('bundle', ['styles', 'scripts'], $.bundle('./app/*.html'));
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images']);
+
+// Default task
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+// Connect
+gulp.task('connect', $.connect.server({
+    root: ['dist'],
+    port: 9000,
+    livereload: true
+}));
+
+
+gulp.task('bower', function() {
+    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+        .pipe(gulp.dest('dist/bower_components/'));
+
+});
+
+gulp.task('json', function() {
+    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+
+// Watch
+gulp.task('watch', ['connect'], function () {
+
+    // Watch .json files
+    gulp.watch('app/scripts/**/*.json', ['json']);
+
+    // Watch .html files
+    gulp.watch('app/*.html', ['html']);
+
+    // Watch .scss files
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+
+    // Watch .stylus files
+    gulp.watch('app/styles/**/*.styl', ['stylus', 'styles']);
+
+    // Watch .coffeescript files
+    gulp.watch('app/scripts/**/*.coffee', ['coffee', 'scripts']);
+
+    // Watch .js files
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+});
